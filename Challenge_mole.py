9ca1db5f075fd933a70929a57ebc74be9b114221
Michel Difils <michel.fils@hotmail.com>
2021-09-21 11:09:04 +0200

    Preprocessing version2 done

--- Challenge_mole.py
+++ Challenge_mole.py
@@ -39,53 +39,73 @@ def image_preparation(img_path: str, img_size: tuple) -> np.ndarray:
     prepared_img = preprocess_input(img)
     return prepared_img
 
-def get_preprocessed_images(img_folder: str, 
-                            img_size: tuple) -> np.ndarray:
-    images = []
+def get_preprocessed_images(df_cancer: pd.DataFrame, 
+                            df_not_cancer: pd.DataFrame, 
+                            img_size: tuple, 
+                            img_folder: str) -> np.ndarray:
+    images_cancer = []
+    images_not_cancer = []
     for fname in os.listdir(img_folder):
-        img_path = os.path.join(img_folder, fname)
-        img = image_preparation(img_path, img_size)
-        images.append(img)
-    return np.vstack(images)
+        if fname[:-4] in df_cancer['image_id'].tolist():
+            img_path = os.path.join(img_folder, fname)
+            img = image_preparation(img_path, img_size)
+            images_cancer.append(img)
+        elif fname[:-4] in df_not_cancer['image_id'].tolist():
+            img_path = os.path.join(img_folder, fname)
+            img = image_preparation(img_path, img_size)
+            images_not_cancer.append(img)
+    return np.vstack(images_cancer), np.vstack(images_not_cancer)
 
-def get_images_labels(df: pd.DataFrame, img_folder: str) -> list:
-    labels = []
-    for fname in os.listdir(img_folder):
-        label = df.loc[df['image_id'] == fname[:-4], 'dx'].iloc[0]
-        labels.append(label)
-    return labels
+def cancer_or_not(dx_value: str) -> str:
+    if dx_value in ['bcc', 'mel']:
+        output = 'cancer'
+    else:
+        output = 'not_cancer'
+    return output
 
 #---------------------Images loading and prepocessing---------------------
+
+# Loading Images metadata from csv
+df_meta = pd.read_csv("Data/HAM10000_metadata.csv")
+df_meta['target'] = df_meta['dx'].apply(cancer_or_not)
+df_cancer = df_meta[df_meta['target']=='cancer']
+df_not_cancer = df_meta[df_meta['target']=='not_cancer']
 
 img_size = (224,224)
 # Images folders
 img_folder_1 = os.path.abspath("./Data/HAM10000_images_part_1")
 img_folder_2 = os.path.abspath("./Data/HAM10000_images_part_2")
 
-images_1 = get_preprocessed_images(img_folder_1, img_size)
-images_2 = get_preprocessed_images(img_folder_2, img_size)
+images_cancer_1, images_not_cancer_1 = get_preprocessed_images(
+    df_cancer,
+    df_not_cancer,
+    img_size,
+    img_folder_1)
+images_cancer_2, images_not_cancer_2 = get_preprocessed_images(
+    df_cancer,
+    df_not_cancer,
+    img_size,
+    img_folder_2)
 
-# Loading Images metadata from csv
-df_meta = pd.read_csv("Data/HAM10000_metadata.csv")
-image_class = df_meta['dx'].unique().tolist()
+# Concatenate cancer images
+images_cancer = np.concatenate([images_cancer_1, images_cancer_2])
 
-# Attaching label to each image
-img_labels_1 = get_images_labels(df_meta, img_folder_1)
-img_labels_2 = get_images_labels(df_meta, img_folder_2)
+# Concatenate not cancer images
+images_not_cancer = np.concatenate([images_not_cancer_1, images_not_cancer_2])
 
-# Labels OneHotEncoding
-#labels_1 = ...
-#labels_2 = ...
+# Make a numpy array for each of the class labels (one hot encoded).
+labels_1 = np.tile([1, 0], (images_cancer.shape[0], 1))
+labels_0 = np.tile([0, 1], (images_not_cancer.shape[0], 1))
 
 # Concatenation
-X = np.concatenate([images_1, images_2])
-#y = np.concatenate([labels_1, labels_2])
+X = np.concatenate([images_cancer, images_not_cancer])
+y = np.concatenate([labels_1, labels_0])
 
 #------------------------Splitting Data----------------------------------
 
 X_train_val, X_test, y_train_val, y_test = train_test_split(
     X, 
-    #y,
+    y,
     test_size=0.2, 
     random_state=42, 
     shuffle=True
